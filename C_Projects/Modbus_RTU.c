cf562f63b9a5360a30e1793ac4c5aaa77709f134
nass179 <nass.shan@googlemail.com>
2024-04-27 17:51:04 +0200

    it works

--- C_Projects/Modbus_RTU.c
+++ C_Projects/Modbus_RTU.c
@@ -2,14 +2,78 @@
 #include <windows.h>
 #include <stdint.h>
 #include "HexToDec.c"
+#include "stdlib.h"
 
 #define DEVICE "COM5" // Change this to your device path
 #define BAUDRATE CBR_19200
 #define MODBUS_SLAVE_ADDR 0x02
 #define MODBUS_READ_FUNC 0x03
-#define MODBUS_START_ADDR 0x07D2//0x08FD
-#define MODBUS_NUM_REGS 0x02 // Number of holding registers to read
+#define MODBUS_START_ADDR 0x08FD//0x07D2//0x08FD
+#define MODBUS_NUM_REGS 0x08 // Number of holding registers to read
 
+
+/*
+unsigned char* byteSort(unsigned char response[]){
+    unsigned char sorted[] = { //16byte start 3
+            response[5],response[6],response[3],response[4],
+            //response[9],response[10],response[7],response[8],
+            //response[13],response[14],response[11],response[12],
+    };
+    return sorted;
+}*/
+void hexToDec(unsigned char sortedresponse[]){
+    char bytes[8];
+    bytes[1] = sortedresponse[0] & 0xF;
+    bytes[0] = sortedresponse[0] >> 4;
+    bytes[3] = sortedresponse[1] & 0xF;
+    bytes[2] = sortedresponse[1] >> 4;
+    bytes[5] = sortedresponse[2] & 0xF;
+    bytes[4] = sortedresponse[2] >> 4;
+    bytes[7] = sortedresponse[3] & 0xF;
+    bytes[6] = sortedresponse[3] >> 4;
+    char num1[8];
+    sprintf(&num1[0],"%X",bytes[0]);
+    sprintf(&num1[1],"%X",bytes[1]);
+    sprintf(&num1[2],"%X",bytes[2]);
+    sprintf(&num1[3],"%X",bytes[3]);
+    sprintf(&num1[4],"%X",bytes[4]);
+    sprintf(&num1[5],"%X",bytes[5]);
+    sprintf(&num1[6],"%X",bytes[6]);
+    sprintf(&num1[7],"%X",bytes[7]);
+    int number[8];
+    for (int i = 0; i < 8; ++i) {
+        if (num1[i] == 'A'){ number[i] = 10;}
+        else if (num1[i] == 'B'){ number[i] = 11;}
+        else if (num1[i] == 'C'){ number[i] = 12;}
+        else if (num1[i] == 'D'){ number[i] = 13;}
+        else if (num1[i] == 'E'){ number[i] = 14;}
+        else if (num1[i] == 'F'){ number[i] = 15;}
+        else if (num1[i] == 0){number[i] = 0;}
+        else{
+            number[i] = (int)(num1[i] - '0');
+        }
+    }
+    int32_t a = 0b00000000000000000000000000000000;
+    /*
+    a += num1[3];
+    a += (num1[2] << 4);
+    a += (num1[1] << 8);
+    a += (num1[0] << 12);
+    */
+    a += number[7];
+    a += (number[6] << 4);
+    a += (number[5] << 8);
+    a += (number[4] << 12);
+    a += (number[3] << 16);
+    a += (number[2] << 20);
+    a += (number[1] << 24);
+    a += (number[0] << 28);
+    for (int i = 0; i < 8; ++i) {
+        printf("%X ",number[i]);
+    }
+    printf("\n");
+    printf("%d \n", a);
+}
 int main() {
     HANDLE hSerial;
     DCB dcbSerialParams = {0};
@@ -57,7 +121,9 @@ int main() {
             MODBUS_START_ADDR & 0xFF,         // Starting address low byte
             (MODBUS_NUM_REGS >> 8) & 0xFF,     // Number of registers to read high byte
             MODBUS_NUM_REGS & 0xFF,            // Number of registers to read low byte
-            0x65,0x75//0x57,0xA8     //false0xD5, 0xF9  // CRC (to be calculated)
+            //0x65,0x75// port 2002
+            //0x57,0xA8     //port 2301
+            0xD7, 0xAF // reg 2301-2308   8 registers
     };
 /*
     // Calculate CRC (just set to 0 for simplicity)
@@ -80,13 +146,21 @@ int main() {
         CloseHandle(hSerial);
         return 1;
     }
+    unsigned char first[] = {response[5],response[6],response[3],response[4]};//Taupunkt
+    unsigned char second[] = {response[9],response[10],response[7],response[8]};//Feuchtigkeit
+    unsigned char third[] = {response[13],response[14],response[11],response[12]};//Druck
+    unsigned char fourth[] = {response[17],response[18],response[15],response[16]};//Temperatur
+    char degree=248;
+    hexToDec(first);
+    printf("%cC Td \n",degree);
+    hexToDec(second);
+    printf("%%rH \n");
+    hexToDec(third);
+    printf("bar \n");
+    hexToDec(fourth);
+    printf("%cC \n", degree);
     printf("Response received: ");
-    //----------------------------------------
-    int Hex[] = {response[2]};
-    HexToDec(Hex,2);
-    //----------------------------------------
-
-    for (DWORD i = 0; i < 8; i++) {
+    for (DWORD i = 0; i < 18; i++) {
         printf("%02X ", response[i]); // Print each byte in hexadecimal format
     }
     printf("\n");
--- C_Projects/shelltest.c
+++ C_Projects/shelltest.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdint.h>
+#include "stdlib.h"
 
 int main() {
     int A = 10;
@@ -15,8 +16,27 @@ int main() {
 
 
 
+    printf("%d \n",a);
+    unsigned char byte = 0x6F;
+    char f = byte & 0xF;
+    char six = byte >> 4;
+    char c[1];
+    sprintf(c,"%X",f);
+    printf("%c",c[0]);
+    /*
+    char hexstring[2];
+    sprintf(hexstring,"%02X",byte);
+    int num[2];
+    for (int i = 0; i < 2; ++i) {
+        if (hexstring[i] == 'A'){
+            num[i] = 10;
+        }
+        else{
+            num[i] = hexstring[i] - '0';
+        }
 
-
-    printf("%d",a);
+    }
+    printf("%c \n", hexstring[0]);
+    printf("%d", num[0]);*/
     return 0;
 }
