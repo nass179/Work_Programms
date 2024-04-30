d53e084ac8e9e7a591d9ec7cb5f55b31b4c8d680
nass179 <nass.shan@googlemail.com>
2024-04-30 12:19:14 +0200

    x

--- Python_Projects/Modbus_Communication.py
+++ Python_Projects/Modbus_Communication.py
@@ -1,29 +1,29 @@
 import serial
 import time
 import struct
-# Define the COM port and baud rate
-COM_PORT = 'COM3'  # Change this to the appropriate COM port on your system
-BAUD_RATE = 19200
-def client():
+
+def client(Com_Port, Baudrate, Functioncode, Slave_ID, Startingregister, NumOfRegs, CRC):
+    hex_start = hex(Startingregister)
+    hex_Regsnum = hex(NumOfRegs)
+    hex_data = '0' + str(Slave_ID) + '0' + str(Functioncode) + '0'  + hex_start[2:5] + '000' + hex_Regsnum[2:3] + CRC
+    print(hex_data)
+    
     try:
-        # Open the serial port
-        ser = serial.Serial(COM_PORT, BAUD_RATE)
-        
-        # Check if the port is open
+        ser = serial.Serial(Com_Port, Baudrate)    
         if ser.is_open:
-            print(f"Connected to {COM_PORT} at {BAUD_RATE} baud.")
+            print(f"Connected to {Com_Port} at {Baudrate} baud.")
 
             # Read data from the serial port
             while True:
                 if ser.in_waiting > 0:
                     data = ser.read(ser.in_waiting)
                     #print("Received:", data[0], data[1] , data.hex()[0:6], data.hex()[6:14])
-                    dataadd = data.hex()[10:14]+data.hex()[6:10]
+                    #dataadd = data.hex()[10:14]+data.hex()[6:10]
                     byte_data1 = bytes.fromhex(data.hex()[6:14])
                     sorted_byte_data = byte_data1[2:] + byte_data1[:2]
                     float = struct.unpack('>f', sorted_byte_data)
                     print(float)
-                hex_data = "020308FD0008D7AF"  
+                  
                 request = bytes.fromhex(hex_data)
                 ser.write(request)
                 
@@ -33,10 +33,10 @@ def client():
         print("Error:", e)
 
     finally:
-        # Close the serial port
         if ser.is_open:
             ser.close()
             print("Serial port closed.")
+            
 
-#client()
+client('COM5',19200,3,2,2301,8,'d7af')
 print("aaa")
--- Python_Projects/window.py
+++ Python_Projects/window.py
@@ -1,9 +1,15 @@
 import tkinter as tk
-import Modbus_Communication
+from tkinter import ttk
+#import Modbus_Communication
 #from Modbus_Communication import client
 window = tk.Tk()
 window.title("ModbusRTU Master")
 window.geometry("600x600")
-label = tk.Label(text = "test")
-label.pack()
+frm = ttk.Frame(window, padding = 10)
+frm.grid()
+ttk.Label(frm, text = 'ModbusRtu').grid(column = 0, row = 0)
+ttk.Button(frm, text="Quitttt", command=window.destroy).grid(column=0, row=10)
+ttk.Button(frm, text="Quit", command=window.destroy).grid(column = 0, row = 1)
+#tk.Label(frm, text = "test").grid(column=0, row=0)
+
 window.mainloop()
