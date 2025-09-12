e00e5b44bbe66b150ad08aba2ec5f6898d5ee065
nass179 <nass.shan@googlemail.com>
2025-09-12 10:33:55 +0200

    fix in excel

--- Python_Projects/main.py
+++ Python_Projects/main.py
@@ -467,7 +467,6 @@ class DataWindowPage(QWidget):
             worksheet.set_margins(top=0, bottom=0, left=0, right=0)
             img_path = 'Briefbogen Aktuell 2021.png'
             worksheet.set_column("A:F", 15.4)
-            worksheet.set_column("A:A", 15)
             worksheet.insert_image('A1', img_path, {'x_scale': 0.7, 'y_scale': 0.8, 'x_offset': 0, 'y_offset': 0})
             
             print(self.main_window.selected_baustelle + " " + "Baustelle")
