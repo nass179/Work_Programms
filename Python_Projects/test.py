030f863e4448eea2ca6e34293650e15016bead8d
nass179 <nass.shan@googlemail.com>
2025-09-15 11:12:01 +0200

    keyboard test

--- Python_Projects/test.py
+++ Python_Projects/test.py
@@ -2,14 +2,15 @@ from pydbus import SessionBus
 from gi.repository import GLib
 
 # Connect to the session bus
+print("Connecting to D-Bus session bus...")
 bus = SessionBus()
-
+print("Connected to D-Bus session bus.")
 # Get the squeekboard object
 osk = bus.get("sm.puri.OSK0", "/sm/puri/OSK0")
-
+print("Obtained OSK object.")
 # Show the keyboard
 osk.SetVisible(True)
-
+print("On-screen keyboard should now be visible.")
 # Wait a bit so you can see it
 import time
 time.sleep(3)
